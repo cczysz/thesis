f54fa6f01301594d58db9ba367b6750340cc60b2
Charles Czysz <charles.czysz@gmail.com>
2015-08-04 10:26:31 -0500

    updated

--- scripts/mask_probes.R
+++ scripts/mask_probes.R
@@ -1,5 +1,6 @@
 probe_dir = '/group/stranger-lab/forCharles/probes_mapping/intersections/'
 r_obj_dir = '/group/stranger-lab/forCharles/probes_mapping/Robjects/'
+home = '/home/t.cczysz'
 
 load(paste(r_obj_dir,'maskBADscoresDF.Robj',sep=''))
 row.names(maskBADscoresDF) <- maskBADscoresDF$probes
@@ -18,31 +19,29 @@ multiMappingIdx[multiMappingIdx > 0] <- 1
 
 probesOverlappingSnps <<- read.table(file='/home/t.cczysz/probes_SNP_overlap.txt',header=F,as.is=T)
 
-# ProbeSNP <- function(probeID,probeIds){
-	#idx <- grep(probeID,probesOverlappingSnps)
-	# if (length(idx)==0) {
-		# variant = 0
-		# return(variant)
-	# } else { variant = 1
-		#return(variant) }
-# }
-ProbeSNP <- function(probeID){
-	if (is.element(probeID,probesOverlappingSnps)) {
-		variant = 0
-		return(variant)
-	} else { variant = 1
-		return(variant) 
-	}
-}
-
-#SNP_overlap <- apply(as.matrix(probeIds,ncol=1),1,ProbeSNP)
 SNP_overlap <- is.element(probeIds,probesOverlappingSnps$V1)
 SNP_overlap <- as.numeric(SNP_overlap)
+
+snp_in_probes <- read.table(file=paste(probe_dir,'probes.dbsnp142.intersect.bed',sep=''),header=F,as.is=T)
+
+maf_snps <- read.table(file=paste(probe_dir,'dbsnp142.overlappingprobes_MAF0.1AFR._EAS_EUR.txt',sep=''),header=F,as.is=T)
+
+probe_to_snp <- cbind(snp_in_probes$V4,snp_in_probes$V10)
+
+maf_probes <- probe_to_snp[is.element(probe_to_snp[,2],c(maf_snps$V1)),1]
+
+maf_probes <- unique(sort(maf_probes))
+
+maf_probes_logical <- as.numeric(is.element(probeIds,maf_probes))
 
 # badScores = runif(length(probeIds),min=0,max=1)
 # load(file='/group/stranger-lab/forCharles/probes_mapping/Robjects/exmask.Robj')
 
-corr_table = data.frame(probeIds,multiMappingIdx,numMultiMap,SNP_overlap)
+corr_table = data.frame(probeIds,multiMappingIdx,numMultiMap,SNP_overlap,maf=maf_probes_logical)
 row.names(corr_table) <- corr_table$probeIds
 
-# corr_table <- merge(corr_table,maskBADscoresDF$quality.score,by=0)
+probes_to_remove <- sort(row.names(subset(corr_table,multiMappingIdx == 1 | maf == 1)))
+
+probe_list <- read.table(file='/home/t.cczysz/probe.info',header=T,as.is=T,fill=T)
+
+kill_list <- probe_list[is.element(probe_list[,1],probes_to_remove),]
