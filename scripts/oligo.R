04e89fddbc441a1f67a364750e2c96b2804ce353
Charles Czysz <charles.czysz@gmail.com>
2015-08-09 18:51:05 -0500

    fixed lm and residuals

--- scripts/oligo.R
+++ scripts/oligo.R
@@ -3,9 +3,10 @@
 
 # This file will source other scripts which each perform probe removal, normalization, PEER analysis, and differential expression
 
-Sys.setenv(R_THREADS=4)
+# Sys.setenv(R_THREADS=4)
 library(oligo)
 library(limma)
+library(ggplot2)
 
 files.dir = "/group/stranger-lab/nicolel/mRNA_expression/CEL_files/CD14/"
 # out.dir = paste(files.dir,'out',sep='')
@@ -14,15 +15,17 @@ setwd(files.dir)
 
 # Normalization files
 phenotype.file = "CD14.Samples.POSTQC.ImmVarFinal.txt"
+peer.factors.f = "/scratch/t.cczysz/peer_factors.Robj"
 # raw_exp_rfile = "cd4_cau_raw.RData"
 # norm_exp_rfile = "cd4_cau_expr.RData"
 
 probe.info = "/home/t.cczysz/HuGeneProbeInfo.csv"
 
-samples <- read.csv(phenotype_file, header=T)
+samples <- read.csv(phenotype.file, header=T)
 
 data.cau <- samples[samples$Race == 'Caucasian', ]
-data.subset <- data.cau[sample(1:length(data.cau), 10), ]
+# data.subset <- data.cau[sample(1:nrow(data.cau), 20), ]
+data.subset <- data.cau
 
 data.ids.subset <- data.subset[,1]
 data.files.subset <- data.subset[,2]
@@ -43,22 +46,41 @@ data.rma.subset <- rma(raw.data.subset)
 	# Vector of sex covariates as `sex`
 # Outputs:
 	# Factors: #(Genes)x#(Factors) matrix of PEER factors for use in linear model
-sex <- as.numeric(data_subset_sex == 'Male')
+sex <- as.numeric(data.sex.subset == 'Male')
 expression <- exprs(data.rma.subset)
-source('/home/t.cri.cczysz/thesis/scripts/peer.r', echo=T)
+
+if (file.exists(file=peer.factors.f)) load(file=peer.factors.f) else {
+  source('/home/t.cri.cczysz/thesis/scripts/peer.R', echo=T)
+  save(peer.factors,file=peer.factors.f) 
+}
 
 ### Calculating Residuals
 # Call make_residuals function in apply over rows of probeset expression
-MakeResiduals <- function(in.row) {
-        residuals(lm(in.row ~ 0 + peer_factors[, 1] + peer_factors[, 2] + peer_factors[, 3] + peer_factors[, 4] + peer_factors[, 5] + peer_factors[, 6] + peer_factors[, 7] + peer_factors[, 8] + peer_factors[, 9] + peer_factors[, 10] + peer_factors[, 11] + peer_factors[, 12] + peer_factors[, 13] + peer_factors[, 14] + peer_factors[, 15] + peer_factors[, 16] + peer_factors[, 17] + peer_factors[, 18] + peer_factors[, 19] + peer_factors[, 20]))
+dim(expression)
+dim(peer.factors)
+head(peer.factors)
+MakeResiduals <- function(input.row) {
+        #residuals(lm(input.row ~ 0 + peer.factors[, 2] + peer.factors[, 3] + peer.factors[, 4] + peer.factors[, 5] + peer.factors[, 6] + peer.factors[, 7] + peer.factors[, 8] + peer.factors[, 9] + peer.factors[, 10] + peer.factors[, 11] + peer.factors[, 12] + peer.factors[, 13] + peer.factors[, 14] + peer.factors[, 15] + peer.factors[, 16] + peer.factors[, 17] + peer.factors[, 18] + peer.factors[, 19] + peer.factors[, 20] + peer.factors[, 21]))
+	residuals(lm(input.row ~ 0 + peer.factors[, -1]))
 }
-residuals <- apply(as.matrix(expression), 1, MakeResiduals)
+
+expr.residuals <- apply(expression, 1, MakeResiduals)
+expr.residuals <- t(expr.residuals)
+print(expr.residuals[1:5,1:10])
+dim(expr.residuals)
 # DE files
 
-samples <- as.factor(sex)
+samples <- as.factor(data.sex.subset)
 design <- model.matrix(~0 + samples)
 colnames(design) <- c("Female","Male")
-fit <- lmFit(exp, design)
+fit <- lmFit(expr.residuals, design)
 contrast.matrix <- makeContrasts(mf = Male - Female, levels=design)
 contrast.fit <- contrasts.fit(fit, contrast.matrix)
 eb.fit <- eBayes(contrast.fit, robust=TRUE)
+print(topTable(eb.fit, number=100))
+
+g = ggplot(data=data.frame(eb.fit),aes(x=coefficients,y=lods,color=as.factor(chrs),label=chrs)) 
+
+pdf('/home/t.cri.cczysz/volcano.pdf')
+g + geom_text() + xlab("fold change") + ylab("log odds")
+dev.off() 
