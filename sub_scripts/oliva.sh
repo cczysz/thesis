d214266f43e20c7f05214ea9af89fac2ed79b392
Charles Czysz <charles.czysz@gmail.com>
2015-08-13 10:53:39 -0500

    script to run meri's filtering script

---
+++ sub_scripts/oliva.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#PBS -N czysz_mod_oliva
+#PBS -S /bin/bash
+#PBS -l walltime=30:00:00
+#PBS -l nodes=1:ppn=4
+#PBS -l mem=16gb
+
+#PBS -o $HOME/
+#PBS -e $HOME/mod_oliva.err
+
+module load R/3.1.0
+
+R CMD BATCH --no-save --no-restore /home/t.cri.cczysz/thesis/scripts/generateFilteredGeneExp2.R
