040489ce904e4e7b1c9b537c4b92b893a71fccb3
Charles Czysz <charles.czysz@gmail.com>
2015-08-17 15:47:19 -0500

    added cov for mean

--- scripts/peer.R
+++ scripts/peer.R
@@ -2,11 +2,12 @@ library(peer)
 
 RunPeer <- function(expression, k=20, covs) {
 	model = PEER()
-	PEER_setCovariates(model,as.matrix(covs))
 	
 	# Expression must be in NxG. N number of samples, G number of genes
 	PEER_setPhenoMean(model,t(expression))
 	PEER_setNk(model,k)
+	PEER_setAdd_mean(model, TRUE)
+	PEER_setCovariates(model,as.matrix(covs)+1)
 	PEER_update(model)
 	peer.factors = PEER_getX(model)
 	return(peer.factors)
